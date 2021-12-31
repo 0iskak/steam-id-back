a1e430cd873443fa68ae9ce9be7a9efc531dcfd4
0iskak <57086286+0iskak@users.noreply.github.com>
2021-12-31 06:51:33 +0600

    Update App.java

--- app/src/main/java/steam/id/back/App.java
+++ app/src/main/java/steam/id/back/App.java
@@ -7,7 +7,7 @@ import static spark.Spark.*;
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        port(Integer.parseInt(System.getProperty("PORT")));
+        port(8002);
         get("/profile/:user", (req, res) -> UserUtil.getUser(req.params(":user")));
     }
 }
